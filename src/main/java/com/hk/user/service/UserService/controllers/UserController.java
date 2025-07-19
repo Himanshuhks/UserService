50d8fa40e5f6c374ae1197847db04796dbdac3ed
Himanshu-Github <himanshuhks14@gmail.com>
2025-07-19 15:28:08 +0530

    CRUD operations

--- src/main/java/com/hk/user/service/UserService/controllers/UserController.java
+++ src/main/java/com/hk/user/service/UserService/controllers/UserController.java
@@ -1,13 +1,47 @@
 package com.hk.user.service.UserService.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hk.user.service.UserService.models.User;
+import com.hk.user.service.UserService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/greet")
+    @Autowired
+    private UserRepository userRepository;
+
+//    @GetMapping("greet")
+    @RequestMapping(path = {"greet", "hello"}, method = RequestMethod.GET)
     public String greetings() {
         return "Hello, World !!";
+    }
+
+    @PostMapping("user")
+    public ResponseEntity<User> addUser(@RequestBody User newUser) {
+        User user = userRepository.save(newUser);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") String userId) {
+        Optional<User> user = userRepository.findById(Long.valueOf(userId));
+        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("user")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User updatedUser = userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("user/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") String userId) {
+        userRepository.deleteById(Long.valueOf(userId));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/hk/user/service/UserService/models/User.java
@@ -0,0 +1,53 @@
+package com.hk.user.service.UserService.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
